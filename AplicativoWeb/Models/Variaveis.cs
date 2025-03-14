95462afd20f1a0ea6572a943607b33a6fa5edfd3
Thiago Thomasi <156266186+thomasithiago@users.noreply.github.com>
2025-03-13 22:15:30 -0300

    ProjetoWeb

---
+++ AplicativoWeb/Models/Variaveis.cs
@@ -0,0 +1,50 @@
+﻿namespace AplicativoWeb.Models
+{
+    public class Variaveis
+    {
+        //Tipos Implicitos
+        // var teste = 10;
+
+        //Anotação de Tipos
+
+        public int userCount = 10;
+
+        //Uma variável pode ser declarada e não inicializada
+
+        public int totalCount;
+
+        //Constantes
+        //Para declarar uma constatbte utilizamos a palvra-chave "CONST"
+        //No entanto, a "CONST" deve ser inicializada quando declarada 
+        const int interestRate = 10;
+
+        public byte byteMin = byte.MinValue;
+        public byte byteMax = byte.MaxValue;
+        public short shortMin = short.MinValue;
+        public short shortMax = short.MaxValue;
+        public int intMin = int.MinValue;
+        public int intMax = int.MaxValue;
+        public uint uintMin = uint.MinValue;
+        public uint uintMax = uint.MaxValue;
+        public long longMin = long.MinValue;
+        public long longMax = long.MaxValue;
+
+
+
+
+
+        //O Método Construtor é invocado na instanciação do Objeto por meio da palavra reservada "new"
+        //Por regra, o Construtor sempre tem o mesmo nome da Classe 
+
+        //Procedimento
+        public Variaveis()
+        {
+            totalCount = 0;
+
+            //Tipo Implicito
+            //A palavra chave "VAR" se encarrega de definir o tipo de variável na instrução de atribuição
+            var signalStrength = 22;
+            var companyName = "THIAGOS";
+        }
+    }
+}
